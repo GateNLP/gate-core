b094ad24b95da2d73a22e2edb31861013e117f8c
Mark A. Greenwood <m.a.greenwood@sheffield.ac.uk>
2017-11-15 19:47:51 +0000

    an svg tick icon

---
+++ src/main/java/gate/resources/img/svg/TickIcon.java
@@ -0,0 +1,275 @@
+package gate.resources.img.svg;
+
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.image.*;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ * This class has been automatically generated using <a
+ * href="http://englishjavadrinker.blogspot.com/search/label/SVGRoundTrip">SVGRoundTrip</a>.
+ */
+@SuppressWarnings("unused")
+public class TickIcon implements
+		javax.swing.Icon {
+		
+	private static Color getColor(int red, int green, int blue, int alpha, boolean disabled) {
+		
+		if (!disabled) return new Color(red, green, blue, alpha);
+		
+		int gray = (int)(((0.30f * red) + (0.59f * green) + (0.11f * blue))/3f);
+		
+		gray = Math.min(255, Math.max(0, gray));
+		
+		//This brightens the image the same as GrayFilter
+		int percent = 50;		
+		gray = (255 - ((255 - gray) * (100 - percent) / 100));
+
+		return new Color(gray, gray, gray, alpha);
+	}
+	
+	/**
+	 * Paints the transcoded SVG image on the specified graphics context. You
+	 * can install a custom transformation on the graphics context to scale the
+	 * image.
+	 * 
+	 * @param g
+	 *            Graphics context.
+	 */
+	public static void paint(Graphics2D g, boolean disabled) {
+        Shape shape = null;
+        Paint paint = null;
+        Stroke stroke = null;
+        Area clip = null;
+         
+        float origAlpha = 1.0f;
+        Composite origComposite = g.getComposite();
+        if (origComposite instanceof AlphaComposite) {
+            AlphaComposite origAlphaComposite = 
+                (AlphaComposite)origComposite;
+            if (origAlphaComposite.getRule() == AlphaComposite.SRC_OVER) {
+                origAlpha = origAlphaComposite.getAlpha();
+            }
+        }
+        
+	    Shape clip_ = g.getClip();
+AffineTransform defaultTransform_ = g.getTransform();
+//  is CompositeGraphicsNode
+float alpha__0 = origAlpha;
+origAlpha = origAlpha * 1.0f;
+g.setComposite(AlphaComposite.getInstance(3, origAlpha));
+Shape clip__0 = g.getClip();
+AffineTransform defaultTransform__0 = g.getTransform();
+g.transform(new AffineTransform(1.0f, 0.0f, 0.0f, 1.0f, -0.0f, -0.0f));
+clip = new Area(g.getClip());
+clip.intersect(new Area(new Rectangle2D.Double(0.0,0.0,16.0,16.0)));
+g.setClip(clip);
+// _0 is CompositeGraphicsNode
+float alpha__0_0 = origAlpha;
+origAlpha = origAlpha * 1.0f;
+g.setComposite(AlphaComposite.getInstance(3, origAlpha));
+Shape clip__0_0 = g.getClip();
+AffineTransform defaultTransform__0_0 = g.getTransform();
+g.transform(new AffineTransform(1.0f, 0.0f, 0.0f, 1.0f, -177.65850830078125f, -355.1869201660156f));
+// _0_0 is CompositeGraphicsNode
+float alpha__0_0_0 = origAlpha;
+origAlpha = origAlpha * 1.0f;
+g.setComposite(AlphaComposite.getInstance(3, origAlpha));
+Shape clip__0_0_0 = g.getClip();
+AffineTransform defaultTransform__0_0_0 = g.getTransform();
+g.transform(new AffineTransform(1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f));
+// _0_0_0 is TextNode of '✓'
+shape = new GeneralPath();
+((GeneralPath)shape).moveTo(180.53458, 364.34076);
+((GeneralPath)shape).quadTo(181.0303, 364.34076, 181.28671, 365.16125);
+((GeneralPath)shape).quadTo(181.79953, 366.69974, 182.02176, 366.69974);
+((GeneralPath)shape).quadTo(182.18414, 366.69974, 182.36363, 366.44333);
+((GeneralPath)shape).quadTo(185.97047, 360.6741, 189.0303, 357.11);
+((GeneralPath)shape).quadTo(189.82518, 356.18692, 191.56021, 356.18692);
+((GeneralPath)shape).quadTo(191.97047, 356.18692, 192.10722, 356.26382);
+((GeneralPath)shape).quadTo(192.25253, 356.34076, 192.25253, 356.45187);
+((GeneralPath)shape).quadTo(192.25253, 356.63135, 191.82518, 357.16125);
+((GeneralPath)shape).quadTo(186.87646, 363.11, 182.64569, 369.72537);
+((GeneralPath)shape).quadTo(182.35509, 370.18692, 181.44057, 370.18692);
+((GeneralPath)shape).quadTo(180.51749, 370.18692, 180.35509, 370.11);
+((GeneralPath)shape).quadTo(179.91919, 369.9134, 179.32945, 368.14417);
+((GeneralPath)shape).quadTo(178.66278, 366.18692, 178.66278, 365.68265);
+((GeneralPath)shape).quadTo(178.66278, 365.14417, 179.56021, 364.64844);
+((GeneralPath)shape).quadTo(180.10722, 364.34076, 180.53458, 364.34076);
+((GeneralPath)shape).closePath();
+paint = getColor(0, 0, 0, 255, disabled);
+g.setPaint(paint);
+g.fill(shape);
+origAlpha = alpha__0_0_0;
+g.setTransform(defaultTransform__0_0_0);
+g.setClip(clip__0_0_0);
+float alpha__0_0_1 = origAlpha;
+origAlpha = origAlpha * 1.0f;
+g.setComposite(AlphaComposite.getInstance(3, origAlpha));
+Shape clip__0_0_1 = g.getClip();
+AffineTransform defaultTransform__0_0_1 = g.getTransform();
+g.transform(new AffineTransform(1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f));
+// _0_0_1 is TextNode of ''
+origAlpha = alpha__0_0_1;
+g.setTransform(defaultTransform__0_0_1);
+g.setClip(clip__0_0_1);
+origAlpha = alpha__0_0;
+g.setTransform(defaultTransform__0_0);
+g.setClip(clip__0_0);
+origAlpha = alpha__0;
+g.setTransform(defaultTransform__0);
+g.setClip(clip__0);
+g.setTransform(defaultTransform_);
+g.setClip(clip_);
+
+	}
+	
+	public Image getImage() {
+		BufferedImage image =
+            new BufferedImage(getIconWidth(), getIconHeight(),
+                    BufferedImage.TYPE_INT_ARGB);
+    	Graphics2D g = image.createGraphics();
+    	paintIcon(null, g, 0, 0);
+    	g.dispose();
+    	return image;
+	}
+
+    /**
+     * Returns the X of the bounding box of the original SVG image.
+     * 
+     * @return The X of the bounding box of the original SVG image.
+     */
+    public static int getOrigX() {
+        return 2;
+    }
+
+    /**
+     * Returns the Y of the bounding box of the original SVG image.
+     * 
+     * @return The Y of the bounding box of the original SVG image.
+     */
+    public static int getOrigY() {
+        return 1;
+    }
+
+	/**
+	 * Returns the width of the bounding box of the original SVG image.
+	 * 
+	 * @return The width of the bounding box of the original SVG image.
+	 */
+	public static int getOrigWidth() {
+		return 16;
+	}
+
+	/**
+	 * Returns the height of the bounding box of the original SVG image.
+	 * 
+	 * @return The height of the bounding box of the original SVG image.
+	 */
+	public static int getOrigHeight() {
+		return 16;
+	}
+
+	/**
+	 * The current width of this resizable icon.
+	 */
+	int width;
+
+	/**
+	 * The current height of this resizable icon.
+	 */
+	int height;
+	
+	/**
+	 * Should this icon be drawn in a disabled state
+	 */
+	boolean disabled = false;
+
+	/**
+	 * Creates a new transcoded SVG image.
+	 */
+	public TickIcon() {
+        this(getOrigWidth(),getOrigHeight(),false);
+	}
+	
+	public TickIcon(boolean disabled) {
+        this(getOrigWidth(),getOrigHeight(),disabled);
+	}
+	
+	/**
+	 * Creates a new transcoded SVG image with the given dimensions.
+	 *
+	 * @param size the dimensions of the icon
+	 */
+	public TickIcon(Dimension size) {
+		this(size.width, size.height, false);
+	}
+	
+	public TickIcon(Dimension size, boolean disabled) {
+		this(size.width, size.height, disabled);
+	}
+
+	public TickIcon(int width, int height) {
+		this(width, height, false);
+	}
+	
+	public TickIcon(int width, int height, boolean disabled) {
+		this.width = width;
+		this.height = height;
+		this.disabled = disabled;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.swing.Icon#getIconHeight()
+	 */
+    @Override
+	public int getIconHeight() {
+		return height;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.swing.Icon#getIconWidth()
+	 */
+    @Override
+	public int getIconWidth() {
+		return width;
+	}
+
+	public void setDimension(Dimension newDimension) {
+		this.width = newDimension.width;
+		this.height = newDimension.height;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.swing.Icon#paintIcon(java.awt.Component, java.awt.Graphics,
+	 * int, int)
+	 */
+    @Override
+	public void paintIcon(Component c, Graphics g, int x, int y) {
+		Graphics2D g2d = (Graphics2D) g.create();
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.translate(x, y);
+						
+		Area clip = new Area(new Rectangle(0, 0, this.width, this.height));		
+		if (g2d.getClip() != null) clip.intersect(new Area(g2d.getClip()));		
+		g2d.setClip(clip);
+
+		double coef1 = (double) this.width / (double) getOrigWidth();
+		double coef2 = (double) this.height / (double) getOrigHeight();
+		double coef = Math.min(coef1, coef2);
+		g2d.scale(coef, coef);
+		paint(g2d, disabled);
+		g2d.dispose();
+	}
+}
+
